94d90e76185769c3106354a6bf5c5f072690fe5d
RichardCao <create0818@163.com>
2014-11-22 13:21:19 +0800

    2 thread

--- pcapcount.py
+++ pcapcount.py
@@ -1,57 +1,93 @@
 import pcap
 import dpkt
+import threading
+import time
 
-try:
-    pc = pcap.pcap("lo")
-#    print pc
-    packetscount = 0
-    flowcounts = {}
-    for ts, pkt in pc:
-        packetscount = packetscount + 1
-        print packetscount
-        p = dpkt.ethernet.Ethernet(pkt)
-        if p == None
-            continue
-        if p.data == None
-            continue
-#        print p
-#        print "\n"
-#        print p.data
-#        print "\n"
-#        print p.data.data
-#        print "\n"
-#        print p.data.data.data
-#        print "\n"
-        if hasattr(p.data, "src"):
-            src='%d.%d.%d.%d' % tuple(map(ord,list(p.data.src)))
-        else:
-            src= "........"
-        if hasattr(p.data, "src"):
-            dst='%d.%d.%d.%d' % tuple(map(ord,list(p.data.dst)))
-        else:
-            dst= "........"
-        if hasattr(p.data, "data"):
-            if hasattr(p.data.data, "sport"):
-                sport = p.data.data.sport
+
+class sendData(threading.Thread):
+
+    def __init__(self, listeninstance, interval, outfilepre):
+        threading.Thread.__init__(self)
+        self.listeninstance = listeninstance
+        self.interval = interval
+        self.outfilepre = outfilepre
+
+    def run(self):
+        while True:
+            print listeninstance
+            print len(listeninstance.flow_count)
+            if(listeninstance != None and len(listeninstance.flow_count) > 0): 
+                print "bbb"
+                outfile = self.outfilepre + time.strftime("%Y-%m-%d-%H-%M-%S")
+                print outfile
+                print self.listeninstance.flow_count
+                out = open(outfile, 'w')
+                out.write(str(listeninstance.packetscount))
+                out.write(str( listeninstance.pc.stats()))
+                out.write("\n")
+                sorted_flow_count = sorted(listeninstance.flow_count.iteritems(), key = lambda asd:asd[1], reverse = True)
+                for obj in sorted_flow_count:
+                    out.write(obj[0] + " " + str(obj[1]) + "\n")
+                out.write("\n")                
+                out.close()
+            time.sleep(self.interval)
+
+class listenInterface(threading.Thread):
+
+    def __init__(self, interface):
+        threading.Thread.__init__(self)
+        self.interface = interface
+        self.packetscount = 0
+        self.flow_count = {}
+        self.pc = None
+    
+    def run(self):
+        self.startListen()
+
+    def startListen(self):
+        try:
+            self.pc = pcap.pcap(self.interface)
+        except:
+           print "error"
+        for ts, pkt in self.pc:
+            self.packetscount = self.packetscount + 1
+            print self.packetscount
+            p = dpkt.ethernet.Ethernet(pkt)
+            if p == None:
+                continue
+            if p.data == None:
+                continue
+            if hasattr(p.data, "src"):
+                src ='%d.%d.%d.%d' % tuple(map(ord, list(p.data.src)))
+            else: 
+                src= "........"
+            if hasattr(p.data, "dst"):
+                dst='%d.%d.%d.%d' % tuple(map(ord, list(p.data.dst)))
             else:
-                sport= -1
-            if hasattr(p.data.data, "dport"):
-                dport = p.data.data.dport
+                dst= "........"
+            if hasattr(p.data, "data"):
+                if hasattr(p.data.data, "sport"):
+                    sport = p.data.data.sport
+                else:
+                    sport= -1
+                if hasattr(p.data.data, "dport"):
+                    dport = p.data.data.dport
+                else:
+                    dport= -1
+            if self.flow_count.has_key(src + "-" + dst):
+                self.flow_count[src + "-" + dst] = self.flow_count[src + "-" + dst] + 1;
             else:
-                dport= -1
-        if flowcounts.has_key(src + "-" + dst):
-            flowcounts[src + "-" + dst] = flowcounts[src + "-" + dst] + 1;
-        else:
-            flowcounts[src + "-" + dst] = 1;
-#        print 'From: %s:%d, To: %s:%d' % (src,sport,dst,dport)
-#        print pc.stats()
-except:
-   19890818 
-finally:
-    out = open('result.txt', 'w')
-    out.write(str(packetscount))
-    sortedflowcounts = sorted(flowcounts.iteritems(), key = lambda asd:asd[1], reverse = True)
-    for obj in sortedflowcounts:
-        out.write(obj[0] + " " + str(obj[1]) + "\n")
-    out.write("\n")
-    out.close()
+                self.flow_count[src + "-" + dst] = 1;
+            print "len=%d" % len(self.flow_count)
+
+if __name__ == "__main__":
+    block = threading.RLock()
+    print 1
+    listeninstance = listenInterface("eth1")
+    print 2
+    sendinstance = sendData(listeninstance, 1, "output")
+    print 3
+    sendinstance.start()
+    print 4
+    listeninstance.start()
+    print 5
