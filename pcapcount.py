b705fd8bf62084d754e748d171fb88e3a209221d
RichardCao <create0818@163.com>
2014-11-26 12:56:36 +0800

    ok-v1.0

--- pcapcount.py
+++ pcapcount.py
@@ -2,44 +2,47 @@ import pcap
 import dpkt
 import threading
 import time
+import copy
 
 
 class sendData(threading.Thread):
 
-    def __init__(self, listeninstance, interval, outfilepre):
+    def __init__(self, listeninstance, interval, outfilepre, myblock):
         threading.Thread.__init__(self)
         self.listeninstance = listeninstance
         self.interval = interval
         self.outfilepre = outfilepre
+	self.myblock = myblock
 
     def run(self):
         while True:
-            print listeninstance
-            print len(listeninstance.flow_count)
-            if(listeninstance != None and len(listeninstance.flow_count) > 0): 
-                print "bbb"
+            print len(self.listeninstance.flow_count)
+            if(self.listeninstance != None and len(self.listeninstance.flow_count) > 0): 
+		self.myblock.acquire()
+		#flow_count_f = copy.deepcopy(listeninstance.flow_count) copy also works as tuple and int are unchangable
+		flow_count_f = self.listeninstance.flow_count.copy()
+		self.listeninstance.flow_count.clear()
+		self.myblock.release()
                 outfile = self.outfilepre + time.strftime("%Y-%m-%d-%H-%M-%S")
-                print outfile
-                print self.listeninstance.flow_count
                 out = open(outfile, 'w')
-                out.write(str(listeninstance.packetscount))
-                out.write(str( listeninstance.pc.stats()))
-                out.write("\n")
-                sorted_flow_count = sorted(listeninstance.flow_count.iteritems(), key = lambda asd:asd[1], reverse = True)
+                out.write(str(listeninstance.packetscount) + "\n")
+                out.write(str( listeninstance.pc.stats()) + "\n")
+                sorted_flow_count = sorted(flow_count_f.iteritems(), key = lambda asd:asd[1], reverse = True)
                 for obj in sorted_flow_count:
-                    out.write(obj[0] + " " + str(obj[1]) + "\n")
+                    out.write(obj[0][0] + " " + obj[0][1] + " " + str(obj[1]) +  "\n")
                 out.write("\n")                
                 out.close()
             time.sleep(self.interval)
 
 class listenInterface(threading.Thread):
 
-    def __init__(self, interface):
+    def __init__(self, interface, myblock):
         threading.Thread.__init__(self)
         self.interface = interface
         self.packetscount = 0
         self.flow_count = {}
         self.pc = None
+	self.myblock = myblock
     
     def run(self):
         self.startListen()
@@ -74,20 +77,16 @@ class listenInterface(threading.Thread):
                     dport = p.data.data.dport
                 else:
                     dport= -1
-            if self.flow_count.has_key(src + "-" + dst):
-                self.flow_count[src + "-" + dst] = self.flow_count[src + "-" + dst] + 1;
+	    self.myblock.acquire()
+            if self.flow_count.has_key((src, dst)):
+                self.flow_count[(src, dst)] = self.flow_count[(src, dst)] + 1;
             else:
-                self.flow_count[src + "-" + dst] = 1;
-            print "len=%d" % len(self.flow_count)
+                self.flow_count[(src, dst)] = 1;
+	    self.myblock.release()
 
 if __name__ == "__main__":
-    block = threading.RLock()
-    print 1
-    listeninstance = listenInterface("eth1")
-    print 2
-    sendinstance = sendData(listeninstance, 1, "output")
-    print 3
+    myblock = threading.RLock()
+    listeninstance = listenInterface("eth1", myblock)
+    sendinstance = sendData(listeninstance, 2, "output", myblock)
     sendinstance.start()
-    print 4
     listeninstance.start()
-    print 5
